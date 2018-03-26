815125079b224303af8d37f8851575ea3cff3cf9
ryanrauch <rauch.ryan@gmail.com>
2018-03-25 22:40:48 -0500

    ios provisioning

--- ApproxiMATE/ApproxiMATE/MapPage.xaml.cs
+++ ApproxiMATE/ApproxiMATE/MapPage.xaml.cs
@@ -48,6 +48,7 @@ namespace ApproxiMATE
             customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(30.400992, -97.723013), Distance.FromMiles(0.1)));
 
             Content = customMap;
+
             //RestService service = new RestService();
             //List<Region> Regions = service.GetRegionsAsync().Result;
 
